c4efe899e4ea77a15497c49d3eba0cdae774e3c1
Aaron Au <aaron.au@me.com>
2022-04-18 23:26:46 -0600

    add new url form page and implement a random string generator

--- express_server.js
+++ express_server.js
@@ -1,24 +1,36 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; //default port 8080
 
 //use ejs as templating engine
 app.set("view engine", "ejs");
 
+//use body parse middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-//index page
-app.get('/', (req, res) => {
-  res.send("Hello");
-});
+//generates a random 6 character string
+const generateRandomString = function() {
+  const alphaNumericList = 'ABCDEFGHIJKLMNOPQRSTUVXWYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const stringLength = 6;
+  let randomString = "";
 
-//urls page
-app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase }; //need to send variables as objects to EJS template
-  res.render("urls_index", templateVars);
+  for (let i = 0; i < stringLength; i++) {
+    const randomIndex = Math.floor(Math.random() * alphaNumericList.length);
+    randomString += alphaNumericList[randomIndex];
+  }
+  return randomString;
+};
+
+
+//new url page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 //show shortened url
@@ -32,10 +44,28 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//urls page
+app.post('/urls', (req, res) => {
+  console.log((req.body));
+  res.send("OK");
+});
+
+//urls page
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase }; //need to send variables as objects to EJS template
+  res.render("urls_index", templateVars);
+});
+
 //hello page... not needed?
 app.get("/hello", (request, response) => {
   response.send("<html><body>Hello <b>World</b></body></html>");
 });
+
+//index page
+app.get('/', (req, res) => {
+  res.send("Hello");
+});
+
 
 //leave listener at the bottom by convention
 app.listen(PORT, () => {
