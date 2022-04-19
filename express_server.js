2ddc50c7b6464ee5079246d3dab6b008c525767b
Aaron Au <aaron.au@me.com>
2022-04-18 21:53:46 -0600

    add route to show shortened url

--- express_server.js
+++ express_server.js
@@ -10,19 +10,29 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//index page
 app.get('/', (req, res) => {
   res.send("Hello");
 });
 
+//urls page
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase }; //need to send variables as objects to EJS template
   res.render("urls_index", templateVars);
 });
 
+//show shortened url
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+//urls.json page
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//hello page... not needed?
 app.get("/hello", (request, response) => {
   response.send("<html><body>Hello <b>World</b></body></html>");
 });
